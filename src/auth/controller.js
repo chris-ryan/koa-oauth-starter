39f5bd461fe45cc10552b378c059885c273ebb17
Chris Ryan <chris.ryan@ctc.usyd.edu.au>
2018-10-12 19:33:16 +1100

    added oauth2orize functions to auth controller

--- src/auth/controller.js
+++ src/auth/controller.js
@@ -1,16 +1,62 @@
 import * as oauth2orize from 'oauth2orize';
-import { getCollection } from '../../db/index';
+import passport from 'koa-passport';
+import uid from 'uid2';
+import * as url from 'url';
+import { getCollection } from '../db/index';
 import { fileStringReplace } from '../utils/file';
+
+class AuthorizationCode {
+  constructor(code, clientId, redirectURI, userId, scope) {
+    this.code = code;
+    this.userId = userId;
+    this.clientId = clientId;
+    this.scope = scope;
+    this.createdAt = new Date();
+  }
+  async save () {
+    const collection = await getCollection('auth.codes').catch(err => console.error(err));
+    let result = await collection.insertOne(this).catch(err => done(err));
+    return result;
+  }
+}
 
 const authServer = oauth2orize.createServer();
 
-export async function authRequest (ctx) {
-  console.log(ctx.query);
+// Register the functions required by oauth2orize
+authServer.grant(oauth2orize.grant.code(async (client, redirectURI, user, ares, done) => {
+  const code = uid(16);
+  var ac = new AuthorizationCode(code, client.id, redirectURI, user.id, ares.scope);
+  console.log(ac.save());
+  // ac.save(function(err) {
+  //   if (err) { return done(err); }
+  //   return done(null, code);
+  // });
+}))
+
+authServer.serializeClient((client, callback) => {
+  callback(null, client.id);
+});
+
+authServer.deserializeClient(async (id, done) => {
+  console.log('deserializing client');
+  const collection = await getCollection('auth.clients').catch(err => console.error(err));
+  let client = await collection.findOne({'id': id}).catch(err => done(err));
+  if (!client) { return done(null, false); }
+  return done(null, client);
+});
+
+// authRequest is written as express-style middleware for oauth2orize compatibility
+export async function authRequest (req, res, next) {
+  // parse the url to populate req.query
+  const url_parts = url.parse(req.url, true);
+  req.query = url_parts.query;
   // check for required query parameters
-  if (ctx.query.response_type && ctx.query.client_id && ctx.query.redirect_uri) {
+  if (req.query.response_type && req.query.client_id && req.query.redirect_uri) {
     // check the user is authenticated
-    if (!ctx.isAuthenticated()) ctx.redirect('/auth/login');
-    server.authorize(async function(clientID, redirectURI, done) {
+    //if (!ctx.isAuthenticated()) ctx.redirect('/auth/login');
+    console.log('calling authorize');
+    return authServer.authorize(async (clientID, redirectURI, done) => {
+    console.log('looking for the client');
       // find the client db record
       const collection = await getCollection('auth.clients').catch(err => console.error(err));
       let client = await collection.findOne({'id': clientID}).catch(err => done(err));
@@ -18,13 +64,27 @@ export async function authRequest (ctx) {
       else if (client.redirectUri != redirectURI) { return done(null, false); }
       else done(null, client, client.redirectURI);
       console.log(client);
-    })
+    })(req, res, next);
   } else {
-    ctx.throw(400);
+    throw new Error('Missing parameters');
   }
 }
 
 export async function loginUser (ctx) {
+  console.log(`authenticating user: ${ctx.req.user}`);
+  return passport.authenticate('local', (err, user, info, status) => {
+    if (user) {
+      ctx.login(user);
+      console.log('redirecting to auth/authorize');
+      ctx.redirect(`/auth/authorize?response_type=${ctx.query.response_type}&client_id=${ctx.query.client_id}&redirect_uri=${ctx.query.redirect_uri}`);
+    } else {
+      ctx.status = 400;
+      ctx.body = { status: 'error' };
+    }
+  })(ctx);
+}
+
+export async function renderLogin (ctx) {
   // check for required query parameters
   if (ctx.query.response_type && ctx.query.client_id && ctx.query.redirect_uri) {
     const queryString = `response_type=${ctx.query.response_type}&client_id=${ctx.query.client_id}&redirect_uri=${ctx.query.redirect_uri}`;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,19 +1,19 @@
 import KoaRouter from 'koa-router';
-import passport from 'koa-passport';
 import fs from 'fs';
-import { loginUser } from '../auth/controller';
-import { authRequest, userStatus } from '../api/users/controller';
+import koaConnect from 'koa-connect';
+import { authRequest, loginUser, renderLogin } from '../auth/controller';
+import { userStatus } from '../api/users/controller';
 
 const authRoutes = new KoaRouter();
 
-authRoutes.get('/auth/authorize', authRequest)
+authRoutes.get('/auth/authorize', koaConnect(authRequest));
 
 // authRoutes.post('/auth/authorize/decision', async (ctx) => {
   
 // })
 
 // /auth/login -> loginUser: Authenticates the user and redirects to /auth/authorize
-authRoutes.get('/auth/login', loginUser)
+authRoutes.get('/auth/login', renderLogin)
 
 authRoutes.get('/auth/logout', async (ctx) => {
   if (ctx.isAuthenticated()) {
@@ -27,19 +27,6 @@ authRoutes.get('/auth/logout', async (ctx) => {
 
 authRoutes.get('/auth/status', userStatus);
 
-authRoutes.post('/auth/login', async (ctx) => {
-  console.log('authenticating user login');
-  return passport.authenticate('local', (err, user, info, status) => {
-    if (user) {
-      ctx.login(user);
-      console.log(`ctx.req.user: ${ctx.req.user}`);
-      console.log('redirecting to auth/status');
-      ctx.redirect('/auth/status');
-    } else {
-      ctx.status = 400;
-      ctx.body = { status: 'error' };
-    }
-  })(ctx);
-});
+authRoutes.post('/auth/login', loginUser);
 
 export { authRoutes }
